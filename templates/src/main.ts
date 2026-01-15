a013b0eb2a51820af76c04c5f9d870152f817020
ahmed1911 <demha.bauer@gmail.com>
2026-01-15 01:04:53 +0100

    mega confetto

--- templates/src/main.ts
+++ templates/src/main.ts
@@ -281,10 +281,42 @@ function showFile(index: number) {
 
   // Confetti on final slide
   if (item.path.includes('Abschluss')) {
+    const duration = 3 * 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min: number, max: number) {
+      return Math.random() * (max - min) + min;
+    }
+
+    const interval: any = setInterval(function() {
+      const timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      const particleCount = 100 * (timeLeft / duration);
+      // since particles fall down, start a bit higher than random
+      confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+      confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+      
+      // Add some center blasts
+      confetti({
+        particleCount: 50,
+        spread: 100,
+        origin: { y: 0.6 },
+        startVelocity: 55,
+        scalar: 1.2
+      });
+    }, 250);
+    
     confetti({
-      particleCount: 150,
-      spread: 70,
+      particleCount: 500,
+      spread: 160,
       origin: { y: 0.6 },
+      startVelocity: 70,
+      scalar: 1.5
     });
   }
 
